b47152649e54a89c88aeb8b161b50e90ea865130
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-02-16 17:23:52 +0300

    implemented navigationUp and fragment labels

--- app/src/main/java/ru/nikitaartamonov/rickandmorty/ui/main/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/rickandmorty/ui/main/MainActivity.kt
@@ -1,8 +1,9 @@
 package ru.nikitaartamonov.rickandmorty.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import ru.nikitaartamonov.rickandmorty.R
 import ru.nikitaartamonov.rickandmorty.databinding.ActivityMainBinding
@@ -10,14 +11,29 @@ import ru.nikitaartamonov.rickandmorty.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val navController by lazy {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navHost.navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHost = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHost.navController
+        setupNavigationUi()
+    }
+
+    private fun setupNavigationUi() {
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.charactersFragment, R.id.episodesFragment, R.id.locationsFragment)
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
