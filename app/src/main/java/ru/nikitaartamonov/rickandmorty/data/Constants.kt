988f1953bb3d8d61930be38d60502aa7e84ffe05
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-02-23 22:30:43 +0300

    added URL_START constants

--- app/src/main/java/ru/nikitaartamonov/rickandmorty/data/Constants.kt
+++ app/src/main/java/ru/nikitaartamonov/rickandmorty/data/Constants.kt
@@ -4,5 +4,8 @@ class Constants {
     companion object {
         const val ENTITY_PAGE_SIZE = 20
         const val EMPTY_RESPONSE_MESSAGE = "HTTP 404"
+        const val CHARACTER_URL_START = "https://rickandmortyapi.com/api/character/"
+        const val LOCATION_URL_START = "https://rickandmortyapi.com/api/location/"
+        const val EPISODE_URL_START = "https://rickandmortyapi.com/api/episode/"
     }
 }
