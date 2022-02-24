c9a1f4c6cbbb2f32c646978e4779dc2f2d8b29cc
Nikita Artamonov <nikitaartamonov@mail.ru>
2022-02-24 06:39:12 +0300

    added splash screen

--- app/src/main/java/ru/nikitaartamonov/rickandmorty/ui/main/MainActivity.kt
+++ app/src/main/java/ru/nikitaartamonov/rickandmorty/ui/main/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_RickAndMorty)
         setContentView(binding.root)
 
         setupNavigationUi()
